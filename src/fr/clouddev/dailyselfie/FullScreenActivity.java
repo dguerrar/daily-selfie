044700918c54a47ffa09ca6277e96705fddca230
Charles-Marie Ambrosetti <deathkatana@gmail.com>
2014-11-17 22:22:52 +0100

    Async load of bitmaps

--- src/fr/clouddev/dailyselfie/FullScreenActivity.java
+++ src/fr/clouddev/dailyselfie/FullScreenActivity.java
@@ -1,8 +1,11 @@
 package fr.clouddev.dailyselfie;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 import android.widget.ImageView;
 
 /**
@@ -22,15 +25,42 @@ public class FullScreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		Log.i(TAG,"onCreate");
 		setContentView(R.layout.full_screen_activity);
+		
 		
 		mBitmap = (ImageView)findViewById(R.id.selfie_bitmap);
 		String selfieName = getIntent().getStringExtra(EXTRA_NAME);
 		Log.i(TAG,"displaying fullscreen for selfie "+selfieName);
 		String filePath = getIntent().getStringExtra(EXTRA_PATH);
 		setTitle(selfieName);
-		mBitmap.setImageBitmap(BitmapUtil.getBitmapFromFile(filePath));
+		new LoadBitmapTask(this,mBitmap).execute(filePath);
+		setProgressBarIndeterminateVisibility(true);
+	}
+	
+	static class LoadBitmapTask extends AsyncTask<String, String, Bitmap> {
+
+		private ImageView mImageView;
+		private Activity mActivity;
 		
+		public LoadBitmapTask(Activity activity,ImageView imageView) {
+			mImageView = imageView;
+			mActivity = activity;
+		}
+		
+		@Override
+		protected Bitmap doInBackground(String... params) {
+			String selfiePath = params[0];
+			
+			return BitmapUtil.getBitmapFromFile(selfiePath);
+		}
+		
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			mImageView.setImageBitmap(result);
+			mActivity.setProgressBarIndeterminateVisibility(false);
+			super.onPostExecute(result);
+		}
 	}
 }
